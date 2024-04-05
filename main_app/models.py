861f06b318ab1ec9c4a4bf0d75fd1a254369c7a2
User Name <kimmyflora270@gmail.com>
2024-04-05 16:32:45 -0700

    5

--- main_app/models.py
+++ main_app/models.py
@@ -1,50 +1,55 @@
 from django.db import models
 from django.urls import reverse
-from datetime import date
 
 # Create your models here.
 
+# you can access the cat with cat_set when you have toy
+class Toy(models.Model):
+    name = models.CharField(max_length=50)
+    color = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('toys_detail', kwargs={'pk': self.id})
 
 class Finch(models.Model):
     name = models.CharField(max_length=100)
     breed = models.CharField(max_length=100)
     description = models.TextField(max_length=250)
     age = models.IntegerField()
+    # add a Many to Many field
+    toys = models.ManyToManyField(Toy)
 
     def __str__(self):
         return self.name
 
+    # this can handle create, update actions
     def get_absolute_url(self):
         # 'detail is refering to the name of the url we want to redirect to'
-        # path('cats/<int:cat_id>/', views.cats_detail, name='detail'),
-        return reverse('detail', kwargs={'finch_id': self.id})
+        
+        return reverse("detail", kwargs={"cat_id": self.id})
         # cat_id refers to the name of the param
         # refer to the route above, and the value,
         # self.id, refers to the cat that was just created
         # on the post request
 
-    def fed_for_today(self):
-        return self.feeding_set.filter(date=date.today()).count() >= len(MEALS)
 
+MEALS = (("B", "Breakfast"), ("L", "Lunch"), ("D", "Dinner"))
 
-MEALS = (
-    ('B', 'Breakfast'),
-    ('L', 'Lunch'),
-    ('D', 'Dinner')
-)
 
 
 class Feeding(models.Model):
-    date = models.DateField('feeding date')
+    date = models.DateField("feeding date")
     meal = models.CharField(
         max_length=1,
         # add the 'choices' field option
         choices=MEALS,
         # set the default value for meal to be 'B'
-        default=MEALS[0][0]
+        default=MEALS[0][0],
     )
 
-    # create a cat_id Foreign Key in psql
     # we don't put the id, django does automatically
     finch = models.ForeignKey(Finch, on_delete=models.CASCADE)
 
--- main_app/views.py
+++ main_app/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from .models import Finch
+from django.views.generic import ListView, DetailView
+from .models import Finch, Toy
 from .forms import FeedingForm
+
 
 # Create your views here.
 
 
 def home(request):
+    finches = Finch.objects.all()
     return render(request, 'home.html')
 
 
@@ -41,10 +44,14 @@ def finches_index(request):
 
 def finches_detail(request, finch_id):
     finch = Finch.objects.get(id=finch_id)
+    id_list = finch.toys.all().values_list('id')
     feeding_form = FeedingForm()
+    toys_finch_doesnt_have = Toy.objects.exclude(id__in=id_list)
+
     return render(request, 'finches/detail.html', {
         'finch': finch,
-        'feeding_form': feeding_form
+        'feeding_form': feeding_form,
+        'toys': toys_finch_doesnt_have
     })
 
 
@@ -55,3 +62,33 @@ def add_feeding(request, finch_id):
         new_feeding.finch_id = finch_id
         new_feeding.save()
     return redirect('detail', finch_id=finch_id)
+
+
+def assoc_toy(request, finch_id, toy_id):
+    print(finch_id, toy_id)
+    finch = Finch.objects.get(id=finch_id)
+    finch.toys.add(toy_id)
+    return redirect('detail', finch_id=finch_id)
+
+
+class ToyList(ListView):
+    model = Toy
+
+
+class ToyDetail(DetailView):
+    model = Toy
+
+
+class ToyCreate(CreateView):
+    model = Toy
+    fields = '__all__'
+
+
+class ToyUpdate(UpdateView):
+    model = Toy
+    fields = ['name', 'color']
+
+
+class ToyDelete(DeleteView):
+    model = Toy
+    success_url = '/toys/'
